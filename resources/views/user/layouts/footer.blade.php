70c68be7405e1b0e370a143ecd7b8e8131108543
shibleshakil <shakil.shible@gmail.com>
2021-05-31 12:14:25 +0600

    complete

--- resources/views/user/layouts/footer.blade.php
+++ resources/views/user/layouts/footer.blade.php
@@ -2,7 +2,7 @@
 <footer class="front-footer">
     <div class="row">
         <div class="col-sm-12">
-            <p>2021 &copy; Subscriptions Plan. Developed by Legato Tec.</p>
+            <p>2021 &copy; Subscriptions Plan. Developed by Legato Technology.</p>
         </div>
     </div>
 </footer>
